1b44ff05c76966f7f755b1c586b5ef0e75730441
RijoanFardous <rijoan23@gmail.com>
2021-10-18 00:33:27 +0600

    Navbar and Footer added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './Pages/SharedComponent/Footer/Footer';
+import Header from './Pages/SharedComponent/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route path="/home"></Route>
+          <Route path="/services"></Route>
+          <Route path="/pricing"></Route>
+          <Route path="/news"></Route>
+          <Route path="/login"></Route>
+          <Route path="/signup"></Route>
+          <Route path="*"></Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/SharedComponent/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from '../../../Images/logo.png';
+import './Header.css';
+
+/*****    React Bootstrap   *****/
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+/*****    React Router   *****/
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" variant="light" expand="lg" sticky="top" className="shadow-sm">
+                <Container>
+                    <Navbar.Brand>
+                        <img
+                            src={logo}
+                            // width="200"
+                            height="50"
+                            className="d-inline-block align-top"
+                            alt="motoring.io logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto me-2">
+                            <NavLink to="/home" className="nav-link" activeClassName="active-link">Home</NavLink>
+                            <NavLink to="/services" className="nav-link" activeClassName="active-link">Services</NavLink>
+                            <NavLink to="/pricing" className="nav-link" activeClassName="active-link">Pricing</NavLink>
+                            <NavLink to="/news" className="nav-link" activeClassName="active-link">News</NavLink>
+                        </Nav>
+                        <Link to="/login" className="nav-login-btn fw-bold">Log In</Link>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
