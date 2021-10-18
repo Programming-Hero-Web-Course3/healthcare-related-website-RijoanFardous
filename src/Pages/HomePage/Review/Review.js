4c7e245bd64b6b18ab33204681724ee12435c49f
RijoanFardous <rijoan23@gmail.com>
2021-10-18 14:22:36 +0600

    testomonial added

--- src/Pages/HomePage/Review/Review.js
+++ src/Pages/HomePage/Review/Review.js
@@ -1,26 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import './Review.css';
 
 const Review = () => {
     const [images, setImages] = useState([]);
+    const [reviews, setReviews] = useState([]);
+
     useEffect(() => {
         fetch("./fakeData/imageGallery.json")
             .then(data => data.json())
             .then(result => setImages(result))
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        fetch("./fakeData/reviews.json")
+            .then(data => data.json())
+            .then(result => setReviews(result))
+    }, []);
 
     return (
-        <div className="py-5 bg-light">
+        <div className="py-5 bg-light testomonial">
             <Container>
                 <Row>
-                    <Col xs={12} md={6}>
+                    <Col xs={12} lg={6}>
+                        {
+                            reviews.map(review => <div className="d-flex mb-3">
+                                <div className="py-3 reveiwer">
+                                    <img src={review.image} alt="" />
+                                </div>
+                                <div className=" review-details py-3">
+                                    <h5>{review.name}, {review.country}</h5>
+                                    <p>{review.message}</p>
+                                </div>
+                            </div>)
+                        }
+                    </Col>
+                    <Col xs={12} lg={5} className="ms-auto image-gallery">
                         <Row className="row g-2">
                             {
                                 images.map(image => <Col xs={4}><img src={image.image} alt="" className="img-fluid" /></Col>)
                             }
                         </Row>
                     </Col>
-                    <Col xs={12} md={6}></Col>
                 </Row>
             </Container>
         </div>
